80ac0c1558d61feb8db0593b730c0a9382062a35
Venkatesh <99860692+venkateshsarapu@users.noreply.github.com>
2025-04-28 18:03:54 +0530

    Create TemperatureConverter.java

---
+++ TemperatureConverter.java
@@ -0,0 +1,12 @@
+package com.example.converter;
+
+public class TemperatureConverter {
+
+    public static double celsiusToFahrenheit(double celsius) {
+        return (celsius * 9 / 5) + 32;
+    }
+
+    public static double fahrenheitToCelsius(double fahrenheit) {
+        return (fahrenheit - 32) * 5 / 9;
+    }
+}
